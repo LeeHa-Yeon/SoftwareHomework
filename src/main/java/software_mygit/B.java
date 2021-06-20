426307e8649e2cf9f522af5c3630f26bbb0f6e43
Lee Ha Yeon <gkdus1613@icloud.com>
2021-06-20 17:53:37 +0900

    devB_branch

--- src/main/java/software_mygit/B.java
+++ src/main/java/software_mygit/B.java
@@ -2,6 +2,6 @@ package software_mygit;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println("A클래스입니당");
+        System.out.println("A클래스입니당 - 브런치 devB_branch 테스트");
     }
 }
